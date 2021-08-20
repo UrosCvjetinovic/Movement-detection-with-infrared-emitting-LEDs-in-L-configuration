5f45f274b995d9cdda2a07a39988528fd4edbb54
Uros <Cvjetinovic>
2021-08-21 01:25:04 +0200

    FreeRTOS_TestProject: I2C echo

---
+++ Diplomski/Workspace/keil/RTOS_project/Sources/common.h
@@ -0,0 +1,13 @@
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#define ITEM_SIZE           sizeof(uint8_t)
+#define QUEUE_SIZE          5
+
+#define RXUARTTASKSTACKSIZE        128
+#define TXUARTTASKSTACKSIZE        128
+#define RXI2CTASKSTACKSIZE        128
+#define TXI2CTASKSTACKSIZE        128
+
+
+#endif /* COMMON_H_ */
