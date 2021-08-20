051507bc5cde5917f579c9518d292ac6bd832c3c
Uros <Cvjetinovic>
2021-08-20 17:27:18 +0200

    FreeRTOS_TestProject: Source files

---
+++ Diplomski/Workspace/keil/RTOS_project/Sources/Application.h
@@ -0,0 +1,36 @@
+#ifndef APPLICATION_H
+#define APPLICATION_H
+
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+
+#define PART_TM4C123GH6PM
+//#define TARGET_IS_TM4C123_
+#define UART_BUFFERED
+
+#include "driverlib\gpio.h"
+#include "driverlib\interrupt.h"
+#include "driverlib\pin_map.h"
+#include "driverlib\rom.h"
+#include "driverlib\rom_map.h"
+#include "driverlib\sysctl.h"
+#include "driverlib\systick.h"
+#include "driverlib\uart.h"
+
+#include "utils\uartstdio.h"
+
+#include "inc\hw_gpio.h"
+#include "inc\hw_ints.h"
+#include "inc\hw_memmap.h"
+#include "inc\hw_nvic.h"
+#include "inc\hw_sysctl.h"
+#include "inc\hw_types.h"
+#include "inc\hw_uart.h"
+
+#endif
